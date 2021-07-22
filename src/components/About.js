a1de4412f18fc1b2fc169e2a6c36cb7b61801fe0
George <71434803+george-swift@users.noreply.github.com>
2021-07-22 16:25:33 +0100

    Add components for SPA navigation

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-12 d-flex align-items-center">
+        <h2>About Page</h2>
+      </div>
+    </div>
+  </div>
+);
+
+export default About;
