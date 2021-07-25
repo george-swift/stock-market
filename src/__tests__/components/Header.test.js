771be25398246f7aed13ffb7e218a242bc43e33b
George <71434803+george-swift@users.noreply.github.com>
2021-07-25 19:21:19 +0100

    Add tests for the Header component

---
+++ src/__tests__/components/Header.test.js
@@ -0,0 +1,56 @@
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import { render, within } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../../components/Header';
+
+describe('Snapshot of the Header component', () => {
+  it('should render correctly', () => {
+    const tree = renderer
+      .create(
+        <Router>
+          <Header />
+        </Router>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Querying the Header component', () => {
+  beforeEach(() => render(
+    <Router>
+      <Header />
+    </Router>,
+  ));
+
+  it('should have a link to the home page in brand name', () => {
+    const brandName = document.querySelector('.navbar-brand');
+    expect(brandName).toBeInTheDocument();
+    const app = within(brandName).getByText('Stock Market');
+    expect(app).toBeInTheDocument();
+    expect(brandName).toHaveAttribute('href', '/');
+  });
+
+  it('should have hamburger for mobile devices', () => {
+    const navBar = document.querySelector('nav');
+    const hamburger = within(navBar).getByRole('button');
+    expect(hamburger).toHaveClass('navbar-toggler');
+  });
+
+  it('should have links to the three main SPA pages', () => {
+    const navBar = document.querySelector('.navbar-nav');
+
+    const routes = navBar.querySelectorAll('.nav-link');
+    expect(routes).toHaveLength(3);
+
+    const homePage = within(navBar).getByText('Home');
+    expect(homePage).toBeInTheDocument();
+
+    const stockListPage = within(navBar).getByText('Stock List');
+    expect(stockListPage).toBeInTheDocument();
+
+    const aboutPage = within(navBar).getByText('About');
+    expect(aboutPage).toBeInTheDocument();
+  });
+});
