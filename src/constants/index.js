88fca7fd85f59f6fb8c36953ccd7203c62251892
George <71434803+george-swift@users.noreply.github.com>
2021-07-26 13:50:13 +0100

    Add exchange markets and action to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,6 +6,8 @@ export const FETCH_ETF_LIST_FAILED = 'FETCH_ETF_LIST_FAILED';
 
 export const FETCH_ETF_LIST_SUCCEEDED = 'FETCH_ETF_LIST_SUCCEEDED';
 
+export const FILTER_MARKETS = 'FILTER_MARKETS';
+
 export const FILTER_LISTING = 'FILTER_LISTING';
 
 export const FILTER_NASDAQ = 'FILTER_NASDAQ';
@@ -18,6 +20,8 @@ export const FETCH_NASDAQ_SUCCEEDED = 'FETCH_NASDAQ_SUCCEEDED';
 
 export const UI = { isLoading: false, error: null };
 
-export const FILTER = { listing: '', nasdaq: '' };
+export const FILTER = { markets: 'All Markets', listing: '', nasdaq: '' };
+
+export const EXCHANGE = ['New York Stock Exchange', 'Nasdaq Global Select'];
 
 export const sortByValue = (data) => data.sort((item1, item2) => item2.price - item1.price);
